4a6434600a10a01fba1d974fa3c15e14951fd1eb
Thomas Stagg <thomstagg@gmail.com>
2023-10-11 10:46:31 +0200

    Add project files.

---
+++ substitution_cipher/substitution_cipher.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <string>
+
+//Stores User Phrase
+std::string user_phrase{};
+
+//Cipher Strings
+std::string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+std::string key{ "XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr" };
+
+//Pauses execution until user input
+void pause()
+{
+    std::cin.clear();
+    std::cin.sync();
+    std::cin.get();
+}
+
+//Clears console screen
+void clear()
+{
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+//checks for valid response
+std::string valid_menu_choice()
+{
+    std::string const valid_options("sSeEdDcCqQ");
+    std::string input{};
+
+    std::cin >> input;
+    while (input.size() != 1 || valid_options.find(input) == std::string::npos)
+    {
+        std::cin.clear();
+        std::cout << "\nInput invalid, please enter a valid menu selection : ";
+        std::cin >> input;
+    }
+    return input;
+}
+
+//checks for valid response
+std::string valid_continue()
+{
+    std::string const valid_options("yYnN");
+    std::string input{};
+
+    std::cin >> input;
+    while (input.size() != 1 || valid_options.find(input) == std::string::npos)
+    {
+        std::cin.clear();
+        std::cout << "\nInput invalid, please enter a valid character : ";
+        std::cin >> input;
+    }
+    return input;
+}
+
+//Prints Menu to console
+void print_menu()
+{
+    if (user_phrase.length() == 0)
+    {
+	    std::cout << "\nYou currently have no phrase stored" << std::endl;
+    }
+    else
+    {
+        std::cout << "\nYour Phrase is: " << user_phrase << std::endl;
+    }
+    std::cout << "\nS - Enter a phrase for encryption" << std::endl;
+    std::cout << "\nE - Encrypt phrase" << std::endl;
+    std::cout << "\nD - Decrypt phrase" << std::endl;
+    std::cout << "\nC - Clear stored phrase" << std::endl;
+	std::cout << "\nEnter your choice: ";
+}
+
+//Gets words from user to be stored in the user_phrase string
+void store_user_phrase()
+{
+    if (user_phrase.length() > 0)
+    {
+	    std::cout << "A phrase is already stored" << std::endl;
+    }
+	if(user_phrase.length() == 0)
+	{
+        bool running{ true };
+        while (running)
+        {
+	        std::cout << "Enter the words you would like to be encrypted or decrypted: ";
+            pause();
+            std::getline(std::cin, user_phrase);
+            clear();
+            if(user_phrase.length() > 0)
+            {
+	            std::cout << "Words added successfully!" << std::endl;
+                running = false;
+            }
+        }
+	}
+}
+
+//Encrypts user phrase
+void encrypt_phrase()
+{
+	std::cout << "Not Implemented yet!" << std::endl;
+}
+
+//Decrypts user phrase
+void decrypt_phrase()
+{
+    std::cout << "Not Implemented yet!" << std::endl;
+}
+
+//Clears the user_phrase string
+void clear_phrase()
+{
+    std::string user_selection{};
+    std::cout << "\nAre you sure you would like to clear your stored phrase?" << std::endl;
+    std::cout << "\nIf yes press 'Y' or press 'N' to return to the menu : ";
+    user_selection = valid_continue();
+    clear();
+    if (user_selection == "y" || user_selection == "Y")
+    {
+        user_phrase = {};
+        std::cout << "\n---------------------------------------" << std::endl;
+        std::cout << "||     Phrase cleared successfully!  ||" << std::endl;
+        std::cout << "---------------------------------------" << std::endl;
+    }
+}
+
+//Processes all user inputs until valid quit key is detected
+void process_user_inputs()
+{
+    std::string user_selection{};
+    bool running{ true };
+    while (running)
+    {
+        print_menu();
+        user_selection = valid_menu_choice();
+        clear();
+        while (running)
+        {
+            if (user_selection == "s" || user_selection == "S")
+            {
+                store_user_phrase();
+                break;
+            }
+            if (user_selection == "e" || user_selection == "E")
+            {
+                encrypt_phrase();
+                break;
+            }
+            if (user_selection == "d" || user_selection == "D")
+            {
+                decrypt_phrase();
+                break;
+            }
+            if (user_selection == "c" || user_selection == "C")
+            {
+                clear_phrase();
+                break;
+            }
+            if (user_selection == "q" || user_selection == "Q")
+            {
+                running = false;
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::cout << "\nWelcome to the Substitution Cipher" << std::endl;
+    process_user_inputs();
+    std::cout << "\nGoodbye!" << std::endl;
+	std::cout << std::endl;
+	return 0;
+}
